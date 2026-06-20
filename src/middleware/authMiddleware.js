090057fb046f9e1732be11bac2d0c3b6338815dd
avantikaag29h4 <avantikaagrawal153@gmail.com>
2026-06-20 18:30:32 +0530

    changes in authMiddleware file

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -12,7 +12,7 @@ const authMiddleware = (req,res,next) => {
 
     try{
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decode;
+        req.user = decoded;
         next();
     }
     catch(err){
